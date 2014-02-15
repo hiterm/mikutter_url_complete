796316e718d6f22ffd521f51cb213dd31401b235
htlsne <htlsne@users.noreply.github.com>
2014-02-15 13:23:40 +0900

    NowPlayingを付けるコマンドも作った

--- mikutter_url_complete.rb
+++ mikutter_url_complete.rb
@@ -26,4 +26,29 @@ Plugin.create(:mikutter_url_complete) do
       end
     end
   end
+
+  command(:url_nowplaying,
+          name: 'URLからNowPlaying',
+          condition: lambda{ |opt| true },
+          visible: true,
+          role: :postbox
+          ) do |opt|
+    message = Plugin[:gtk].widgetof(opt.widget).widget_post.buffer.text
+    URI.extract(message, %w{http https}).uniq.each do |url|
+      unless url.match(/(\.jpg|\.jpeg|\.png)/)
+        charset = nil
+        html = open(url) do |f|
+          charset = f.charset
+          f.read
+        end
+        doc = Nokogiri::HTML.parse(html, nil, charset)
+        title = doc.title
+        if url.include?("www.youtube.com")
+          title = title[0, title.length - 10]
+        end
+        message.gsub!(url, "\#NowPlaying \"#{title}\"" + " " + url)
+        Plugin[:gtk].widgetof(opt.widget).widget_post.buffer.text = message
+      end
+    end
+  end
 end
